6150955b8d981b596d11ae0545dc5e8814d33498
lavanyapavi <lavanyav2979@gmail.com>
2017-07-12 12:03:45 +0530

    Create kkk.java

---
+++ kkk.java
@@ -0,0 +1,10 @@
+public static void sort(String[] a) {
+    int n = a.length;
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j > 0; j--) {
+            if (a[j-1].compareTo(a[j]) > 0)
+                exch(a, j, j-1);
+            else break;
+        }
+    }
+}
